21782cbaed59e59c16b731dca8c72b57f23f5873
Mahsa Goudarzi <mahsagoodarzi1999@gmail.com>
2022-04-19 21:49:13 +0430

    built a button component for cart button in the header

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+
+export default function HeaderCartButton(props) {
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>3DV</span>
+    </button>
+  );
+}
