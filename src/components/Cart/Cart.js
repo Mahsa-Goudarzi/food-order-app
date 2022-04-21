a42bde8554b35e8f5b6e8d2e938bec149bcb0483
Mahsa Goudarzi <mahsagoodarzi1999@gmail.com>
2022-04-21 22:53:39 +0430

    show cart items with name and price in the cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,16 +2,26 @@ import React, { useContext } from "react";
 import CartContext from "../../store/cart-context";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
 
 export default function Cart(props) {
   const cartCtx = useContext(CartContext);
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
+  function handleAddCartItem(item) {}
+
+  function handleRemoveCartItem(id) {}
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
-        <li key={item.id}>{item.name}</li>
+        <CartItem
+          key={item.id}
+          item={item}
+          onAdd={handleAddCartItem.bind(null, item)}
+          onRemove={handleRemoveCartItem.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
