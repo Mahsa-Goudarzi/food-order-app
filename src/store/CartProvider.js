90945a98492f30a52260c3c733da028b75fa76e2
Mahsa Goudarzi <mahsagoodarzi1999@gmail.com>
2022-04-21 23:23:52 +0430

    change the reducer function for adding items to cart so we can add items properly

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,10 +8,28 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updatedItem = state.items.concat(action.item);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
-    return { items: updatedItem, totalAmount: updatedTotalAmount };
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
+    return { items: updatedItems, totalAmount: updatedTotalAmount };
   }
   return defaultCartState;
 };
