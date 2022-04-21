a04a7d9f2170677873fd7bbf75893a0e3c69c328
Mahsa Goudarzi <mahsagoodarzi1999@gmail.com>
2022-04-21 19:03:02 +0430

    build a Context for the Cart state and provide it to all parts of the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -15,13 +16,13 @@ function App() {
   }
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onHideCart={handleHideCart} />}
       <Header onShowCart={handleShowCart} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
---
+++ src/store/CartProvider.js
@@ -0,0 +1,20 @@
+import React from "react";
+import CartContext from "./cart-context";
+
+export default function CartProvider(props) {
+  function handleAddItemToCart(item) {}
+
+  function handleRemoveItemFromCart(id) {}
+
+  const cartContext = {
+    item: [],
+    totalAmount: 0,
+    addItem: handleAddItemToCart,
+    removeItem: handleRemoveItemFromCart,
+  };
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+}
