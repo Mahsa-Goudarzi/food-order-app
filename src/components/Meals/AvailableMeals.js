895433743782d72d085d0c36b1544d0a5f7e6b1d
Mahsa Goudarzi <mahsagoodarzi1999@gmail.com>
2022-05-08 19:31:53 +0430

    Put meal data in firebase and fetch meal data from firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,37 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
 export default function AvaiableMeals() {
-  const mealsList = DUMMY_MEALS.map((meal) => {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const loadedMeals = [];
+      const response = await fetch(
+        "https://mahsa-react-movie-app-default-rtdb.firebaseio.com/meals.json"
+      );
+      const responseData = await response.json(); //data coming back from firebase is an Object. we want an array
+
+      for (const key in responseData) {
+        loadedMeals.push({ id: key, ...responseData[key] });
+      }
+
+      setMeals(loadedMeals);
+    };
+
+    fetchMeals();
+  }, []);
+
+  const mealsList = meals.map((meal) => {
     return <MealItem meal={meal} key={meal.id} />;
   });
 
