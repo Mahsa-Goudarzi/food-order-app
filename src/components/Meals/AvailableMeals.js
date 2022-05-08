db48c3d91b4755769cf9d5daf9c02a104852fe1c
Mahsa Goudarzi <mahsagoodarzi1999@gmail.com>
2022-05-08 19:42:43 +0430

    Show a loading message before loading meal data

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,6 +5,7 @@ import MealItem from "./MealItem/MealItem";
 
 export default function AvaiableMeals() {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchMeals = async () => {
@@ -19,10 +20,19 @@ export default function AvaiableMeals() {
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
     fetchMeals();
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
 
   const mealsList = meals.map((meal) => {
     return <MealItem meal={meal} key={meal.id} />;
